123b13e1daa1c38bdc3696f619b6397ddeabbdbd
vel murugan mani <velmmtech@live.in>
2020-08-28 10:18:53 +0530

    MySQL Schema Updated

--- velsecurity/bin/main/schema-mysql.sql
+++ velsecurity/bin/main/schema-mysql.sql
@@ -8,3 +8,11 @@ username VARCHAR(255) NOT NULL unique,
 name VARCHAR(100) NOT NULL,
 password VARCHAR(1000) NOT NULL
 ) ENGINE=InnoDB;
+
+USE springbootsecurity;
+CREATE TABLE customers(  
+cus_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,  
+cus_login_id VARCHAR(255) NOT NULL unique,  
+cus_name VARCHAR(100) NOT NULL,
+cus_password VARCHAR(1000) NOT NULL
+) ENGINE=InnoDB;
